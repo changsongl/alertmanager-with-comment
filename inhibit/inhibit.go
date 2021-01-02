3c41f56b6c6aca8dfbcd3d755242f34ba9afd3fe
Jason li <changsongliqd@gmail.com>
2021-01-02 22:22:28 +0800

    完成抑制模块备注

--- inhibit/inhibit.go
+++ inhibit/inhibit.go
@@ -37,7 +37,7 @@ import (
 // 实现了Muter接口。
 type Inhibitor struct {
 	alerts provider.Alerts // 告警对象，用来获得告警和遍历器
-	rules  []*InhibitRule  // 静默规则切片，里面包含抑制规则的匹配器
+	rules  []*InhibitRule  // 抑制规则切片，里面包含抑制规则的匹配器
 	marker types.Marker    // 告警的标记器，负责标记告警状态，如告警，静默，抑制等等
 	logger log.Logger      // log对象
 
@@ -65,6 +65,8 @@ func NewInhibitor(ap provider.Alerts, rs []*config.InhibitRule, mk types.Marker,
 	return ih
 }
 
+// 运行抑制器，会开始订阅告警。然后开始循环，如果订阅的告警遍历器里面有告警，
+// 则获得告警，并循环每个抑制规则，如果匹配到
 func (ih *Inhibitor) run(ctx context.Context) {
 	// 开始订阅告警
 	it := ih.alerts.Subscribe()
@@ -133,11 +135,14 @@ func (ih *Inhibitor) Run() {
 }
 
 // Stop the Inhibitor's background processing.
+// ---------------------------------------------
+// Stop 停止 Inhibitor 的后台处理
 func (ih *Inhibitor) Stop() {
 	if ih == nil {
 		return
 	}
 
+	// 锁住，并调用cancel方法
 	ih.mtx.RLock()
 	defer ih.mtx.RUnlock()
 	if ih.cancel != nil {
@@ -148,12 +153,14 @@ func (ih *Inhibitor) Stop() {
 // Mutes returns true iff the given label set is muted. It implements the Muter
 // interface.
 // -----------------------------------------------------------------------------
-// 禁言方法，通过告警的标签，匹配抑制规则。如果被抑制则返回true。
+// Mutes 禁言方法，通过告警的标签，匹配抑制规则。如果被抑制则返回true。
 func (ih *Inhibitor) Mutes(lset model.LabelSet) bool {
 	fp := lset.Fingerprint()
 
+	// 检查每个告警规则。
 	for _, r := range ih.rules {
-		// 匹配target，如果连target都没有命中可直接去检查下个rule
+		// 匹配target，检查是否有匹配到规则。如果匹配到，则代表自己有可能
+		// 被某个告警规则抑制，如果没有命中可直接去检查下个rule。
 		if !r.TargetMatchers.Match(lset) {
 			// If target side of rule doesn't match, we don't need to look any further.
 			continue
@@ -161,9 +168,10 @@ func (ih *Inhibitor) Mutes(lset model.LabelSet) bool {
 		// If we are here, the target side matches. If the source side matches, too, we
 		// need to exclude inhibiting alerts for which the same is true.
 		// -----------------------------------------------------------------------------
-		// 匹配source，假如匹配成功的
+		// 检查有可能被抑制的这个规则。如果找到这个规则的source的告警，则代表匹配成功。
+		// 这个当前告警应该被抑制掉。
 		if inhibitedByFP, eq := r.hasEqual(lset, r.SourceMatchers.Match(lset)); eq {
-			// 设置当前告警已经被匹配上的source抑制
+			// 设置当前告警被抑制
 			ih.marker.SetInhibited(fp, inhibitedByFP.String())
 			return true
 		}
@@ -179,18 +187,36 @@ func (ih *Inhibitor) Mutes(lset model.LabelSet) bool {
 // labels are equal between the two alerts. This may be used to inhibit alerts
 // from sending notifications if their meaning is logically a subset of a
 // higher-level alert.
+// ---------------------------------------------------------------------------------------
+// InhibitRule 描述一类源source告警应该去抑制掉目标target告警消息。如果根据这个抑制规则，
+// 检查到某两个标签的告警都是相等的，那就会抑制掉target的那个告警。如果两个告警逻辑上是某个
+// 告警是一个高级的子集。那么这个子集告警的通知应该被抑制掉。
+//
+// Ex: 机器DOWN了，会抑制掉机器不可访问的告警。
+//
 type InhibitRule struct {
 	// The set of Filters which define the group of source alerts (which inhibit
 	// the target alerts).
+	// --------------------------------------------------------------------------
+	// 一组过滤器，定义了一组源source告警。可以抑制目标告警。
 	SourceMatchers types.Matchers
+
 	// The set of Filters which define the group of target alerts (which are
 	// inhibited by the source alerts).
+	// --------------------------------------------------------------------------
+	// 一组过滤器，定义了一组目标告警。可以被源告警给抑制掉。
 	TargetMatchers types.Matchers
+
 	// A set of label names whose label values need to be identical in source and
 	// target alerts in order for the inhibition to take effect.
+	// --------------------------------------------------------------------------
+	// 一组标签，代表只有在源告警和目标告警之间，他们的这一组标签值都相等的时候，抑制规则才
+	// 能起到效果。
 	Equal map[model.LabelName]struct{}
 
 	// Cache of alerts matching source labels.
+	// --------------------------------------------------------------------------
+	// 匹配到源告警规则标签的告警缓存
 	scache *store.Alerts
 }
 
@@ -203,6 +229,7 @@ func NewInhibitRule(cr *config.InhibitRule) *InhibitRule {
 		targetm types.Matchers
 	)
 
+	// 循环每一个源告警匹配和目标告警匹配的匹配器和正则匹配器，进行统计
 	for ln, lv := range cr.SourceMatch {
 		sourcem = append(sourcem, types.NewMatcher(model.LabelName(ln), lv))
 	}
@@ -217,11 +244,13 @@ func NewInhibitRule(cr *config.InhibitRule) *InhibitRule {
 		targetm = append(targetm, types.NewRegexMatcher(model.LabelName(ln), lv.Regexp))
 	}
 
+	// 循环每个需要相等的，抑制规则告警标签
 	equal := map[model.LabelName]struct{}{}
 	for _, ln := range cr.Equal {
 		equal[ln] = struct{}{}
 	}
 
+	// 生成抑制对象
 	return &InhibitRule{
 		SourceMatchers: sourcem,
 		TargetMatchers: targetm,
@@ -234,22 +263,40 @@ func NewInhibitRule(cr *config.InhibitRule) *InhibitRule {
 // labels for the given label set. If so, the fingerprint of one of those alerts
 // is returned. If excludeTwoSidedMatch is true, alerts that match both the
 // source and the target side of the rule are disregarded.
+// ---------------------------------------------------------------------------------------
+// hasEqual 检查是否源告警的缓存里，有匹配目标告警标签的告警。如果有的话，符合条件的其中一个告警
+// 会被返回。如果 excludeTwoSidedMatch 是true，同时匹配到源和目标规则的告警将被忽略掉。
+//
+// 这里使用的continue Outer感觉很有争议，和continue起到同样的效果。但是比较难理解。
 func (r *InhibitRule) hasEqual(lset model.LabelSet, excludeTwoSidedMatch bool) (model.Fingerprint, bool) {
 Outer:
 	for _, a := range r.scache.List() {
 		// The cache might be stale and contain resolved alerts.
+		// ------------------------------------------------------
+		// 这个缓存可能有脏数据，包含已经解决的告警，因此判断已经解决，则
+		// 忽略掉。
 		if a.Resolved() {
 			continue
 		}
+		// 判断满足这个源规则的告警，是否和检查的告警，equal的标签都是相等的。
+		// 如果不相等，则继续下个循环。
 		for n := range r.Equal {
 			if a.Labels[n] != lset[n] {
 				continue Outer
 			}
 		}
+
+		// 此时应该已经代表匹配到了抑制规则了，当前正在检查的告警，应该被抑制掉。
+		// 但是这里又多做了一部检查。
+		// 如果当前循环到的告警既满足源规则，也满足目标规则。并且检查的告警也是一样。
+		// 则不会标识被抑制，而是被忽略掉。
 		if excludeTwoSidedMatch && r.TargetMatchers.Match(a.Labels) {
 			continue Outer
 		}
+
+		// 成功被抑制，返回源抑制告警的指纹
 		return a.Fingerprint(), true
 	}
+	// 没有被抑制，返回空指纹
 	return model.Fingerprint(0), false
 }
